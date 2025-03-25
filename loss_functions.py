2d2c1e6dd7a8da6a9c6563075e5f69f95556b6e6
kris-meng <107716786+kris-meng@users.noreply.github.com>
2025-03-25 18:22:37 -0400

    Update loss_functions.py
    i'll implement manifold loss later

--- loss_functions.py
+++ loss_functions.py
@@ -11,3 +11,26 @@ def reconstruction_loss(x, x_hat):
         loss: Mean squared error loss.
     """
     return torch.mean((x - x_hat) ** 2)
+
+def manifold_constrain_loss(S, tau):
+    #will implement later
+    pass
+
+def tot_loss(x, x_hat, S, lamb, tau):
+    """
+    Computes the overall loss.
+
+    Args:
+        x: Original time series.
+        x_hat: Reconstructed time series.
+        S: Series-wise representations.
+        lamb: Balance between reconstruction and constraint.
+        tau: Temperature for similarity.
+
+    Returns:
+        total_loss: Combined loss.
+    """
+    l_reconstruction = reconstruction_loss(x, x_hat)
+    l_constraint = manifold_constraint_loss(S, tau)
+
+    return l_reconstruction + lamb * l_constraint
