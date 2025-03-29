fbd04d1a9455bf18c7c09329ac9b2a8076a864f2
Claudia Farkas <claudia.farkas@outlook.com>
2025-03-29 15:20:21 -0400

    created mlp_decoder.py

---
+++ models/mlp_decoder.py
@@ -0,0 +1,39 @@
+# USAGE IN PAPER DESCRIPTION:
+# The decoder is part of the reconstruciton process. Helps map the aggregated point-wise representation 
+# back to the original time series.
+
+# Paper's Equation Interpretation Steps (see pg.4 "Point-wise Aggregation"):
+# 1. The decoder taked in the aggregated point-wise representation i, 
+# 2. Final reconstruciton of the orignal time series (output) is L * C
+#    where L is the lenght of the time series and C is the number of channels
+# 3. Uses an MLP at each time step.
+# --------------------------------------------------------------------------------------------------------------------------
+import torch
+
+def build_decoder(input_dim, output_dim, hidden_dim = None):
+    """
+    Builds the shared decoder MLP.
+    
+    Args:
+        input_dim: dimension of encoder output, d
+        hidden_dim: hidden layer size, default = input_dim
+        output_dim: output dimension of projector, default = input_dim
+    """
+    hidden_dim = hidden_dim or input_dim
+
+    return torch.nn.Sequential(
+        torch.nn.Linear(input_dim, hidden_dim),
+        torch.nn.ReLU(),
+        torch.nn.Linear(hidden_dim, output_dim)
+    )
+
+
+def apply_decoder(z_hat, decoder):
+    """
+    Applies the decoder.
+    
+    Args:
+        z_hat: the aggregated point-wise time step 
+        decoder (nn.Module): the decoder MLP
+    """
+    return decoder(z_hat)
