13a5624bd10280e3b65536638b92bb161714eb78
kris-meng <107716786+kris-meng@users.noreply.github.com>
2025-04-10 16:33:53 -0400

    Update f_model.py
    changed the input type so the order of the input would be [x0, x0_mask1, x0_mask2, ..., xN-1, xN-1_mask1, xN-1_mask2, ...], added comments about the expected shape size after each operation based on the paper description

--- f_model.py
+++ f_model.py
@@ -1,6 +1,6 @@
 class SimMTMModel_Forecasting(nn.Module):
     def __init__(self, num_channels, d_model=64, n_head=4, n_layers=4, proj_dim=32, num_masked=3, dropout=0.1):
-        super(SimMTMModel, self).__init__()
+        super(SimMTMModel_Forecasting, self).__init__()
         self.num_masked = num_masked
         self.encoder = ChannelIndependentTransformer(
             num_channels=num_channels,
@@ -15,21 +15,24 @@ class SimMTMModel_Forecasting(nn.Module):
 
     def forward(self, seq_x):
         # Geometric masking
-        masked_views, masks = geometric_masking(seq_x.cpu())
+        masked_views, masks = geometric_masking(seq_x.cpu()) # shape: B*num_masked,L,C for the masking
         masked_views = torch.tensor(masked_views).float().to(seq_x.device)
-        inputs = torch.cat([seq_x] + [masked_views[i::self.num_masked] for i in range(self.num_masked)], dim=0)
+        inputs = torch.cat([
+            torch.stack([seq_x[i]] + [masked_views[i * self.num_masked + j] for j in range(self.num_masked)], dim=0)
+            for i in range(seq_x.shape[0])
+        ], dim=0)  # [x0, x0_mask1, x0_mask2, ..., xN-1, xN-1_mask1, xN-1_mask2, ...] shape: B*(1+num_masked),L,C 
 
         # Encoder
-        enc_output = self.encoder(inputs)  # (N*(M+1), L, C, d_model)
+        enc_output = self.encoder(inputs)  # shape: B*(1+num_masked),L,C,d_model
         B_total, L, C, d_model = enc_output.shape
-        enc_output = enc_output.view(B_total, L, -1)  # flatten channels
+        enc_output = enc_output.view(B_total, L, -1)  # flatten channels - shape: B*(1+num_masked),L,C*d_model
 
         # Project to series-wise representations
-        series_repr = enc_output.mean(dim=1)  # (N*(M+1), d_model)
-        series_proj = self.projector(series_repr)
+        series_repr = enc_output.mean(dim=1)  # (N*(M+1), d_model) shape: B*(1+num_masked), C*d_model --> doing this coz paper says the input series_wise_similarity is 1 x d
+        series_proj = self.projector(series_repr) # shape: B*(1+num_masked), proj_dim
 
         # Similarity matrix
-        R = series_wise_similarity(series_proj)
+        R = series_wise_similarity(series_proj) # shape B*(1+num_masked), B*(1+num_masked) --> we get this shape after S*S.T, remember the order of S is [x0, x0_mask1, x0_mask2, ..., xN-1, xN-1_mask1, xN-1_mask2, ...]
 
         # Point-wise reconstruction
         z_point = enc_output.view(B_total, L, C, d_model)
